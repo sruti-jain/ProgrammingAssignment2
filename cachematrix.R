d6899d132ef4950c7b553286037ea9cb59d5cd27
Sruti Jain <sruti.j1806@gmail.com>
2017-05-28 00:24:00 -0500

    Update cachematrix.R
    Printing appropriate display messages

--- cachematrix.R
+++ cachematrix.R
@@ -27,7 +27,7 @@ makeCacheMatrix <- function(x = matrix()) {
 cacheSolve <- function(x, ...) {
     i <- x$getinverse()
     if(!is.null(i)) {
-        message("getting cached data")
+        message("Getting Cached Data")
         return(i)
     }
     m <- x$get()
